63743e19ae07eb9ca472d405ccaf21b5c43b8fc2
Euan Turner <euanoturner@gmail.com>
2021-05-10 12:54:59 +0100

    Added enumerator to linear search

--- searching/py/Linear.py
+++ searching/py/Linear.py
@@ -1,8 +1,23 @@
+import random
 def linear_search(items,val):
     for i in range(len(items)):
         if items[i] == val:
             return i
     return None
+
+def enum_linear_search(size,item):
+    arr = []
+    for i in range(size):
+        arr.append(random.randint(1,100))
+    indices = []
+    arr = enumerate(arr)
+    for i in arr:
+        if i[1] == item:
+            indices.append(i[0])
+    return indices
+
+print(enum_linear_search(100,7))
+
 
 
 
